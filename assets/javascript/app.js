86552893f1c0d1f4049fba7e5017fc3d4bf393e5
Bill <wkernan@gmail.com>
2016-05-23 11:21:11 -0500

    starting on getting the basic functions working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,82 @@
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
-var timer = 30;
-var count = 0;
-var questions = [
-	{
-		question1: "How many arms does an octopus have?",
-		answer1_1: "2",
-		answer1_2: "4",
-		answer1_3: "6",
-		answer1_4: "8",
-		pic: '',
-		answer1_ans: 4
-	},
-	{
-		question2: "When was the war of 1812?",
-		answer2_1: "1812",
-		answer2_2: "1912",
-		answer2_3: "1712",
-		answer2_4: "Never happened",
-		pic: '',
-		answer2_ans: 1
-	}
-];
+$(document).ready(function() {
+
+	var correct = 0;
+	var incorrect = 0;
+	var unanswered = 0;
+	var time = 30;
+	var count = 0;
+	var questions = [
+		{
+			question: "How many arms does an octopus have?",
+			answer1: "2",
+			answer2: "4",
+			answer3: "6",
+			answer4: "8",
+			quest_pic: 'http://lorempixel.com/400/200',
+			answer_pic: '',
+			answer: 4
+		},
+		{
+			question: "When was the war of 1812?",
+			answer1: "1812",
+			answer2: "1912",
+			answer3: "1712",
+			answer4: "Never happened",
+			quest_pic: 'http://lorempixel.com/400/200',
+			answer_pic: '',
+			answer: 1
+		}
+	];
 
 
-$('#start-game').on('click', startGame);
+	$('#start-game').on('click', startGame);
 
-function nextQuestion() {
+	function timer() {
+		time--;
+		$('#timer').html('<h3>Timer: ' + time + 's</h3>');
+	};
 
-}
+	function nextQuestion() {
+		clearInterval(counter);
+		time = 30;
+		count++;
+		displayQuestion();
+	};
 
-function displayQuestion() {
+	function timesUp() {
+		clearInterval(counter);
+		unanswered++;
+		$('#timer').html("<h3>Time's Up!</h3>");
+		setTimeout(nextQuestion, 1000 * 5);
+	};
 
-}
+	function displayQuestion() {
+		$('#quest-image').html('<img src="' + questions[count].quest_pic + '">');
+		$('#question').html('<h3>' + questions[count].question + '</h3>');
+		displayAnswers();
+		counter = setInterval(timer, 1000);
+		setTimeout(timesUp, 1000 * 30);
+	};
 
-function startGame() {
-	$('#start-game').addClass('hide');
-	//$('')
-}
+	function displayAnswers() {
+		$('#answer1').html(questions[count].answer1);
+		$('#answer2').html(questions[count].answer2);
+		$('#answer3').html(questions[count].answer3);
+		$('#answer4').html(questions[count].answer4);
+	};
 
-function answer(num) {
+	function startGame() {
+		$('#start-game').addClass('hide');
+		$('.list-group').removeClass('hide');
+		$('#timer').removeClass('hide');
+		displayQuestion();
+		//$('')
+	};
 
-}
+	function answer(num) {
 
-$('#answer_1').on('click', answer(1))
+	};
+
+	$('#answer_1').on('click', answer(1));
+
+});
