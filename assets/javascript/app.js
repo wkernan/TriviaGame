79577a0888541783e47cb5d05dafa688dc03127c
Bill <wkernan@gmail.com>
2016-05-23 22:23:31 -0500

    added more gifs and audio, still working on ui

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,17 +5,21 @@ var time = 30;
 var count = 0;
 var timeCounter;
 var timesUpCounter;
+var backgroundMusic = new Audio('assets/audio/Seinfeld.mp3');
 var questions = [
 	{
-		question: "How many arms does an octopus have?",
-		answer1: "2",
-		answer2: "4",
-		answer3: "6",
-		answer4: "8",
-		quest_pic: 'http://lorempixel.com/400/200',
-		answer_pic: '',
-		answer: "An octopus has 8 arms",
-		answer_num: 4
+		question: "What does Kervorka mean?",
+		answer1: "The smell of the beach.",
+		answer2: "Shrinkage.",
+		answer3: "The scent of a woman.",
+		answer4: "The lure of the animal.",
+		answer_pic: 'assets/images/kramer_thats_true.gif',
+		answer: "The lure of the animal.",
+		answer_num: 4,
+		answerAudio: function() {
+			var audio = new Audio('http://www.wavsource.com/snds_2016-05-22_6159520873604738/tv/seinfeld/kervorka3.wav');
+			audio.play();
+		}
 	},
 	{
 		question: "What was George's desription of Elaine's dancing?",
@@ -23,8 +27,7 @@ var questions = [
 		answer2: "It's a combination of monkey meets Frankenstein.",
 		answer3: "I don't even know how to describe it.",
 		answer4: "Jerry... it was awful.",
-		quest_pic: 'assets/images/elaine_dance.gif',
-		answer_pic: 'assets/images/george_squint.gif',
+		answer_pic: 'assets/images/elaine_dance.gif',
 		answer: "It's more like a full body, dry heave, set to music.",
 		answer_num: 1
 	}
@@ -69,7 +72,7 @@ function timesUp() {
 	clearInterval(timeCounter);
 	unanswered++;
 	$('#result').html("<h3>Time's Up!</h3>");
-	$('#quest-image').html('<img src="' + questions[count].answer_pic + '">')
+	$('#image').html('<img src="' + questions[count].answer_pic + '">')
 	$('#question').html('<h3>' + questions[count].answer + '</h3>')
 	setTimeout(nextQuestion, 1000 * 6);
 	console.log(unanswered);
@@ -92,6 +95,8 @@ function displayAnswers() {
 };
 
 function startGame() {
+	backgroundMusic.pause();
+	backgroundMusic.currentTime = 0;
 	$('#start-game').addClass('hide');
 	$('.list-group').removeClass('hide');
 	$('#timer').removeClass('hide');
@@ -118,11 +123,14 @@ displayQuestion();
 
 function guessAnswer(num) {
 	console.log('works');
+	console.log(questions[count]);
+	questions[count].answerAudio();
 	if(num === questions[count].answer_num) {
 		clearInterval(timesUpCounter);
 		clearInterval(timeCounter);
 		correct++;
 		$('#result').html("<h1>That's Correct!</h1>");
+		$('#image').html("<img src='" + questions[count].answer_pic + "'>");
 		setTimeout(nextQuestion, 1000 * 7);
 	} else {
 		clearInterval(timesUpCounter);
@@ -133,4 +141,7 @@ function guessAnswer(num) {
 	}
 };
 
+$(document).ready(function() {
+	backgroundMusic.play();
+});
 
